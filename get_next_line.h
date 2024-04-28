9c08622ae20a7b2d48445113641f7f3d4e3b3641
Leo Agea <leoagea@gmail.com>
2024-04-29 00:12:24 +0200

    done on 28/04/24

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 22:29:35 by lagea             #+#    #+#             */
-/*   Updated: 2024/04/23 01:13:40 by lagea            ###   ########.fr       */
+/*   Updated: 2024/04/29 00:09:55 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,17 @@
 #include <stdio.h>  //printf
 
 
-#define BUFFER_SIZE 5
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
 /* get_next_line_utils */
 int ft_strlen(const char* str);
-int ft_strchr_index(char *str);
+char *ft_strchr_index(char *str);
 char *ft_strlcat(char *src,char *dst, int n);
 char *ft_strlcpy(char *src, char *dst);
+char	*ft_strdup(char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *s, unsigned int start, size_t len);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,77 +6,144 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 22:29:25 by lagea             #+#    #+#             */
-/*   Updated: 2024/04/23 01:13:30 by lagea            ###   ########.fr       */
+/*   Updated: 2024/04/29 00:02:46 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int ft_strlen(const char* str)
+int	ft_strlen(const char *str)
 {
-    int len;
+	int	len;
 
-    len = 0;
-    if(!str)
-        return 0;
-    while (*str++)
-    {
-        len++;
-    }
-    return len;
+	len = 0;
+	if (!str)
+		return (0);
+	while (*str++)
+	{
+		len++;
+	}
+	return (len);
 }
 
-int ft_strchr_index(char *str)
+char    *ft_strchr_index(char *str)
 {
-    int index;
+	unsigned int	i;
 
-    index = 0;
-    while(*str != '\n' && *str)
-    {
-        str++;
-        index++;
-    }
-    return index + 1;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return ((char *) &str[i]);
+		i++;
+	}
+	if (str[i] == '\n')
+		return ((char *) &str[i]);
+	return (NULL);
 }
 
-char *ft_strlcat(char *src,char *dst, int n)
+char	*ft_strlcat(char *src, char *dst, int n)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(*dst)
-        dst++;
-    while(i < n && *src)
-    {
-        *dst++ = *src++;
-        i++;
-    }
-    *dst = '\0';
-    return dst;
+	i = 0;
+	while (*dst)
+		dst++;
+	while (i < n && *src)
+	{
+		*dst++ = *src++;
+		i++;
+	}
+	*dst = '\0';
+	return (dst);
 }
 
-char *ft_strlcpy(char *src, char *dst)
+char	*ft_strlcpy(char *src, char *dst)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(src[i])
-    {
-        dst[i] = src[i];
-        i++;
-    }
-    dst[i] = '\0';
-    return dst;
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
 }
 
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*res;
+
+	res = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[j])
+		res[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		res[i++] = s2[j++];
+	res[i] = '\0';
+	return (res);
+}
+
+char	*ft_strdup(char *s1)
+{
+	char			*dest;
+	unsigned int	i;
+
+	dest = (char *) malloc(ft_strlen(s1) + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (malloc(1));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
 
 // int main()
 // {
-//     char *stash;
-//     char *buff = "test!";
-
-//     stash = (char *) malloc (sizeof(char) * (BUFFER_SIZE + 1));
-//     ft_strlcpy(buff, stash, BUFFER_SIZE);
-//     printf("%s",stash);
-//     return 0;
+//     // char *stash;
+//     char *buff = "testde \nstrchr";
+//     int len_s = ft_strlen(buff);
+    
+//     // stash = (char *) malloc (sizeof(char) * (BUFFER_SIZE + 1));
+//     char* s = ft_strchr_index(buff);
+//     printf("%s",s);
+//     int index = ft_strlen(s);
+//     printf("%d",len_s - index);
+//     return (0);
 // }
